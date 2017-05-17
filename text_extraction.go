22db97a45eca98994628dd5504cbc0805af7a4b5
Shogo Sekine <gan.gun.kyosho@gmail.com>
2017-05-17 15:10:07 +0000

    ディレクトリ内の全テキストファイルを処理できるように実装

--- text_extraction.go
+++ text_extraction.go
@@ -3,60 +3,59 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"golang.org/x/text/encoding/japanese"
 	"golang.org/x/text/transform"
 )
 
-func readFile() {
+func process() {
 	// ファイルパスを定義
-	fileReadPath := "nuc/data001.txt"
-	f, err := os.Open(fileReadPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "File %s could not be read: %v\n", fileReadPath, err)
-	}
+	fileReadPath := "nuc/"
 
-	// 関数終了時に閉じる
-	defer f.Close()
+	// 読み出し対象のファイルリストを読み込む
+	fileList := readDir(fileReadPath)
 
-	// Scannerで読み込む
-	// EUCJP形式のテキストを自動でデコードする
-	scanner := bufio.NewScanner(transform.NewReader(f, japanese.EUCJP.NewDecoder()))
-	fileWritePath := "nuc/destination.txt"
 	// 出力先ファイルを書き込みモードでオープンする。存在しなければ新規作成する
+	fileWritePath := "nuc/destination.txt"
 	fp, err := os.OpenFile(fileWritePath, os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		panic(err)
 	}
+
 	// 処理終了時に閉じる
 	defer fp.Close()
 
 	writer := bufio.NewWriter(fp)
-	for scanner.Scan() {
-		_, err2 := writer.WriteString(scanner.Text() + "\n")
-		if err2 != nil {
-			panic(err2)
+
+	for _, file := range fileList {
+		// 読み込むファイルのパスを生成する
+		filePath := fileReadPath + file.Name()
+		f, err := os.Open(filePath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "File %s could not be read: %v\n", fileReadPath, err)
 		}
+		// Scannerで読み込む
+		// EUCJP形式のテキストを自動でデコードする
+		scanner := bufio.NewScanner(transform.NewReader(f, japanese.EUCJP.NewDecoder()))
+		// 読み込んだファイルのテキストを1行ずつスキャンして出力先のファイルに追記する
+		for scanner.Scan() {
+			_, err2 := writer.WriteString(scanner.Text() + "\n")
+			if err2 != nil {
+				panic(err2)
+			}
+		}
+		writer.Flush()
 	}
-	// if werr := writer.Err(); werr != nil {
-	// 	fmt.Fprintf(os.Stderr, "FIle %s write error: %v\n", fileWritePath, werr)
-	// }
-	writer.Flush()
+	fmt.Println("finished...")
+}
 
-	//
-	// 1行ずつコンソールに出力する
-	//
-	// for scanner.Scan() {
-	// 	// 読み込んだテキストを表示
-	// 	fmt.Println(scanner.Text())
-	// }
-
-	// if serr := scanner.Err(); serr != nil {
-	// 	fmt.Fprintf(os.Stderr, "FIle %s scan error: %v\n", filePath, err)
-	// }
+func readDir(fileReadPath string) []os.FileInfo {
+	fileList, _ := ioutil.ReadDir(fileReadPath)
+	return fileList
 }
 
 func main() {
-	readFile()
+	process()
 }
